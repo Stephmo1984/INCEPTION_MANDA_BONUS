d13c38b921501a1ddb133941ab21c781a4e02071
smortemo <smortemo@INCEPTION.myguest.virtualbox.org>
2025-03-23 20:29:04 +0100

    mod dockers Buster->Bullseye + add conf mariadb

--- INCEPTION_BONUS_NEW/srcs/requirements/mariadb/tools/init-mariadb.sh
+++ INCEPTION_BONUS_NEW/srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,40 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-# Initialize MySQL database installation
-mysql_install_db
+echo "⏳ Initializing MySQL database..."
 
-# Start MySQL service
-service mysql start
+service mariadb start;
 
-if [ -d "var/lib/mysql/$MYSQL_DATABASE_NAME" ]
-then
-	echo "Database already exists"
+# # Attendre que le service soit complètement démarré
+until mysqladmin ping --silent; do
+    echo "⏳ Waiting for MySQL to be ready..."
+    sleep 3
+done
+
+# Vérifier si la base de données existe déjà
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE_NAME" ]; then
+    echo "✅ Database '$MYSQL_DATABASE_NAME' already exists."
 else
-
-# Secure installation of MySQL
-mysql_secure_installation << _EOF_
-
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
-_EOF_
-
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;"
-
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER_NAME'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
-
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_USER_NAME'@'%';"
-
-mysql -e "FLUSH PRIVILEGES;"
-
+    echo "⚙️  Creating database '$MYSQL_DATABASE_NAME'..."
+    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;"
+    
+    echo "⚙️  Creating user '$MYSQL_USER_NAME'..."
+    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER_NAME'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+    
+    echo "⚙️  Granting privileges..."
+    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_USER_NAME'@'%';"
+    
+	echo "⚙️  alter user root"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+   
+    mysql -e "FLUSH PRIVILEGES;"
 fi
 
-# Stop MySQL service
-service mysql stop
+# Arrêter le service pour permettre le démarrage avec CMD
+echo "🛑 Stopping MySQL service..."
+
+mysqladmin shutdown -u root
+#service mysql stop
 
 exec "$@"
-
